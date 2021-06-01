511262c811b5c781c19c9217e92cb075856843b7
Markyear <71787750+Markyear@users.noreply.github.com>
2021-06-01 23:11:37 +0200

    Test PW Numbers

--- src/main/java/at/ac/fhcampuswien/PasswordValidator.java
+++ src/main/java/at/ac/fhcampuswien/PasswordValidator.java
@@ -3,7 +3,7 @@ package at.ac.fhcampuswien;
 public class PasswordValidator {
 
     public boolean checkPassword(String password){
-        if(password.length() >= 8 && password.length() <= 25 && password.matches("(.*[a-z].*)") && password.matches("(.*[A-Z].*)")){
+        if(password.length() >= 8 && password.length() <= 25 && password.matches("(.*[a-z].*)") && password.matches("(.*[A-Z].*)") && password.matches("(.*\\d.*)")){
             return true;
         }
         else {
