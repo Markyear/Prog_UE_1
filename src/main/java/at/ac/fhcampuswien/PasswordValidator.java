ab68a8e875a97f0d829bdab2874e07bb487d7b0d
Markyear <71787750+Markyear@users.noreply.github.com>
2021-06-01 22:54:00 +0200

    Test PW Lower und Upper Case

--- src/main/java/at/ac/fhcampuswien/PasswordValidator.java
+++ src/main/java/at/ac/fhcampuswien/PasswordValidator.java
@@ -3,13 +3,11 @@ package at.ac.fhcampuswien;
 public class PasswordValidator {
 
     public boolean checkPassword(String password){
-        if(password.length() >= 8 && password.length() <= 25){
+        if(password.length() >= 8 && password.length() <= 25 && password.matches("(.*[a-z].*)") && password.matches("(.*[A-Z].*)")){
             return true;
         }
         else {
             return false;
         }
-
     }
-
 }
